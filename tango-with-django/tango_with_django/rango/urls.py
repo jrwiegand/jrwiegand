b0c75c0aa45580ba7bf9fd52d41223e96829e8cd
Joshua Wiegand <joshwiegand55@gmail.com>
2014-02-26 21:28:12 -0500

    Finished chapter 6

--- tango-with-django/tango_with_django/rango/urls.py
+++ tango-with-django/tango_with_django/rango/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from .views import index, about
+from .views import index, about, category
 
 urlpatterns = patterns('',
-        url(r'^$', index, name='index'),
-        url(r'^about/', about, name='about'),
+    url(r'^$', index, name='index'),
+    url(r'^about/$', about, name='about'),
+    url(r'^category/(?P<category_name_url>\w+)/$', category, name='category'),
 )
--- tango-with-django/tango_with_django/rango/views.py
+++ tango-with-django/tango_with_django/rango/views.py
@@ -1,15 +1,54 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from .models import Category
+from .models import Category, Page
+
+def encode_url(str):
+    return str.replace(' ', '_')
+
+def decode_url(str):
+    return str.replace('_', ' ')
 
 def index(request):
     context = RequestContext(request)
+
     category_list = Category.objects.order_by('-likes')[:5]
+
     context_dict = {'categories': category_list}
+
+    for category in category_list:
+        category.url = encode_url(category.name)
+
+    page_views = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_views
+
     return render_to_response('rango/index.html', context_dict, context)
 
 
 def about(request):
     context = RequestContext(request)
+
     context_dict = {'bold_message': "I am bold font from the context."}
+
     return render_to_response('rango/about.html', context_dict, context)
+
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode_url(category_name_url)
+
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+
+        pages = Page.objects.filter(category=category).order_by('-views')
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
