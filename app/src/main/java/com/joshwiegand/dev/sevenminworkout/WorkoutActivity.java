b3286d14a740231211c0bf15d09ebceaadf897b4
Joshua Wiegand <joshwiegand55@gmail.com>
2015-02-11 21:40:51 -0500

    workout layout set, time to add functionality

--- app/src/main/java/com/joshwiegand/dev/sevenminworkout/WorkoutActivity.java
+++ app/src/main/java/com/joshwiegand/dev/sevenminworkout/WorkoutActivity.java
@@ -2,11 +2,23 @@ package com.joshwiegand.dev.sevenminworkout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class WorkoutActivity extends ActionBarActivity {
+
+    private WorkoutCountDownTimer countDownTimer;
+    private long timeElapsed;
+    private boolean timerHasStarted = false;
+    private Button startB;
+    private TextView text;
+    private TextView timeElapsedView;
+
+    private final long startTime = 50000;
+    private final long interval = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +28,7 @@ public class WorkoutActivity extends ActionBarActivity {
 
         String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
 
-        TextView callingActivityMessage = (TextView) findViewById(R.id.clock_text);
+        final TextView callingActivityMessage = (TextView) findViewById(R.id.clock_text);
 
         callingActivityMessage.setText(" " + previousActivity);
 
@@ -35,4 +47,27 @@ public class WorkoutActivity extends ActionBarActivity {
         finish();
     }
 
+    public class WorkoutCountDownTimer extends CountDownTimer
+    {
+
+        public WorkoutCountDownTimer(long startTime, long interval)
+        {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onFinish()
+        {
+            text.setText("Time's up!");
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished)
+        {
+            text.setText("Time remain:" + millisUntilFinished);
+            timeElapsed = startTime - millisUntilFinished;
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
+        }
+    }
 }
