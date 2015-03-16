b2fd606c4750fba3633a98f992c0d663d880766e
Joshua Wiegand <joshwiegand55@gmail.com>
2015-03-15 20:36:54 -0400

    just started using quasars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,26 @@
 package jmodern;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import static java.util.stream.Collectors.*;
-import static java.util.stream.IntStream.range;
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.strands.Strand;
+import co.paralleluniverse.strands.channels.Channel;
+import co.paralleluniverse.strands.channels.Channels;
 
 public class Main {
-    public static void main(String[] args) {
-        range(0, 100)
-            .mapToObj(i -> randomString(new Random(), 'A', 'Z', 10))
-            .sorted()
-            .collect(groupingBy(name -> name.charAt(0)))
-            .forEach((letter, names) -> System.out.println(letter + "\n\t" + names.stream().collect(joining("\n\t"))));
-    }
+    public static void main(String[] args) throws Exception {
+        final Channel<Integer> ch = Channels.newChannel(0);
 
-    /**
-     * ## The Random String Generator
-     *
-     * This method doesn't do much, except for generating a random string. It:
-     *
-     *  * Generates a random string at a given length, `length`
-     *  * Uses only characters in the range given by `from` and `to`.
-     *
-     * Example:
-     *
-     * ```java
-     * randomString(new Random(), 'a', 'z', 10);
-     * ```
-     *
-     * @param r      the random number generator
-     * @param from   the first character in the character range, inclusive
-     * @param to     the last character in the character range, inclusive
-     * @param length the length of the generated string
-     * @return the generated string of length `length`
-     */
+        new Fiber<Void>(() -> {
+            for (int i = 0; i < 10; i++) {
+                Strand.sleep(100);
+                ch.send(i);
+            }
+            ch.close();
+        }).start();
 
-    public static String randomString(Random r, char from, char to, int length) {
-        return r.ints(from, to + 1).limit(length).mapToObj(x -> Character.toString((char)x)).collect(joining());
+        new Fiber<Void>(() -> {
+            Integer x;
+            while((x = ch.receive()) != null)
+                System.out.println("--> " + x);
+        }).start().join(); // join waits for this fiber to finish
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,8 +5,5 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MainTest {
-    // @Test
-    // public void testTriple() {
-    //     assertThat(Main.triple("AB"), equalTo("ABABAB"));
-    // }
+
 }
