dd47adf5f8466f43ebe82aa8064af1d503fc2a53
Joshua Wiegand <joshwiegand55@gmail.com>
2015-03-15 21:00:18 -0400

    playing with fiber actors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,41 +1,68 @@
 package jmodern;
 
+import co.paralleluniverse.actors.*;
 import co.paralleluniverse.fibers.*;
-import co.paralleluniverse.fibers.io.*;
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.*;
-import java.nio.charset.*;
+import co.paralleluniverse.strands.Strand;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    static final int PORT = 1234;
-    static final Charset charset = Charset.forName("UTF-8");
-
     public static void main(String[] args) throws Exception {
-        new Fiber(() -> {
-            try {
-                System.out.println("Starting server");
-                FiberServerSocketChannel socket = FiberServerSocketChannel.open().bind(new InetSocketAddress(PORT));
-                for (;;) {
-                    FiberSocketChannel ch = socket.accept();
-                    new Fiber(() -> {
-                        try {
-                            ByteBuffer buf = ByteBuffer.allocateDirect(1024);
-                            int n = ch.read(buf);
-                            String response = "HTTP/1.0 200 OK\r\nDate: Fri, 31 Dec 1999 23:59:59 GMT\r\n"
-                                            + "Content-Type: text/html\r\nContent-Length: 0\r\n\r\n";
-                            n = ch.write(charset.newEncoder().encode(CharBuffer.wrap(response)));
-                            ch.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }).start();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        new NaiveActor("naive").spawn();
+        Strand.sleep(Long.MAX_VALUE);
+    }
+
+    static class BadActor extends BasicActor<String, Void> {
+        private int count;
+
+        @Override
+        protected Void doRun() throws InterruptedException, SuspendExecution {
+            System.out.println("(re)starting actor");
+            for(;;) {
+                String m = receive(300, TimeUnit.MILLISECONDS);
+                if (m != null)
+                    System.out.println("Got a message: " + m);
+                System.out.println("I am but a lowly actor that sometimes fails: - " + (count++));
+
+                if (ThreadLocalRandom.current().nextInt(30) == 0)
+                    throw new RuntimeException("darn");
+
+                checkCodeSwap(); // this is a convenient time for a code swap
             }
-        }).start();
-        System.out.println("started");
-        Thread.sleep(Long.MAX_VALUE);
+        }
+    }
+
+    static class NaiveActor extends BasicActor<Void, Void> {
+        private ActorRef<String> myBadActor;
+
+        public NaiveActor(String name) {
+            super(name);
+        }
+
+        @Override
+        protected Void doRun() throws InterruptedException, SuspendExecution {
+            spawnBadActor();
+
+            int count = 0;
+            for(;;) {
+                receive(500, TimeUnit.MILLISECONDS);
+                myBadActor.send("hi from " + self() + " number " + (count++));
+            }
+        }
+
+        private void spawnBadActor() {
+            myBadActor = new BadActor().spawn();
+            watch(myBadActor);
+        }
+
+        @Override
+        protected Void handleLifecycleMessage(LifecycleMessage m) {
+            if (m instanceof ExitMessage && Objects.equals(((ExitMessage) m).getActor(), myBadActor)) {
+                System.out.println("My bad actor has just died of '" + ((ExitMessage) m).getCause() + "'. Restarting.");
+                spawnBadActor();
+            }
+            return super.handleLifecycleMessage(m);
+        }
     }
 }
