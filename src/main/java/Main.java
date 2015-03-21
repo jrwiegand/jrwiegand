fca52db9508dd0ae914d69338ab998e0ab8f721b
Joshua Wiegand <joshwiegand55@gmail.com>
2015-03-21 13:25:34 -0400

    finished with logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,22 @@ import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.Strand;
 import co.paralleluniverse.strands.channels.Channel;
 import co.paralleluniverse.strands.channels.Channels;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
         final Channel<Integer> ch = Channels.newChannel(0);
 
         new Fiber<Void>(() -> {
             for (int i = 0; i < 10; i++) {
                 Strand.sleep(100);
+                log.info("Sending {}", i);
                 ch.send(i);
+                if (i % 10 == 0)
+                    log.warn("Send {} messages", i + 1); // log something
             }
             ch.close();
         }).start();
