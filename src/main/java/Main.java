16b695be2afd97e0a8e59e42364ac719bfbc0582
Joshua Wiegand <joshwiegand55@gmail.com>
2015-03-25 21:55:52 -0400

    working with MX Beans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,11 @@ package jmodern;
 
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.Strand;
-import co.paralleluniverse.strands.channels.Channel;
-import co.paralleluniverse.strands.channels.Channels;
+import co.paralleluniverse.strands.channels.*;
+import java.lang.management.ManagementFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.management.MXBean;
+import javax.management.ObjectName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +14,30 @@ public class Main {
     static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        final Channel<Integer> ch = Channels.newChannel(0);
+        final AtomicInteger counter = new AtomicInteger();
+        final Channel<Object> ch = Channels.newChannel(0);
+
+        // create and register MBean
+        ManagementFactory.getPlatformMBeanServer().registerMBean(new JModernInfo() {
+            @Override
+            public void send(String message) {
+                try {
+                    ch.send(message);
+                } catch (Exception e) {
+                    throw new RunTimeException(e);
+                }
+            }
+
+            @Override
+            public int getNumMessagesReceived() {
+                return counter.get();
+            }
+        }, new ObjectName("jmodern:type=Info"));
 
         new Fiber<Void>(() -> {
             for (int i = 0; i < 1000000; i++) {
                 Strand.sleep(100);
-                log.info("Sending {}", i);
+                log.info("Sending {}", i); //log something
                 ch.send(i);
                 if (i % 10 == 0)
                     log.warn("Send {} messages", i + 1); // log something
@@ -29,5 +50,11 @@ public class Main {
             while((x = ch.receive()) != null)
                 System.out.println("--> " + x);
         }).start().join(); // join waits for this fiber to finish
+    }
+
+    @MXBean
+    public interface JModernInfo {
+        void send(String messaage);
+        int getNumMessagesReceived();
     }
 }
