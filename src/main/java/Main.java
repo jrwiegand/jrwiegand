60ac2917767a1c04bf12574e49c3b97a9fe15469
Joshua Wiegand <joshwiegand55@gmail.com>
2015-04-13 22:21:01 -0400

    finished with jmx and monitoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,58 +3,43 @@ package jmodern;
 import co.paralleluniverse.fibers.Fiber;
 import co.paralleluniverse.strands.Strand;
 import co.paralleluniverse.strands.channels.*;
-import java.lang.management.ManagementFactory;
-import java.util.concurrent.atomic.AtomicInteger;
-import javax.management.MXBean;
-import javax.management.ObjectName;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.codahale.metrics.*;
+import static com.codahale.metrics.MetricRegistry.name;
+import java.util.concurrent.ThreadLocalRandom;
+import static java.util.concurrent.TimeUnit.*;
 
 public class Main {
-    static final Logger log = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) throws Exception {
-        final AtomicInteger counter = new AtomicInteger();
+        final MetricRegistry metrics = new MetricRegistry();
+        JmxReporter.forRegistry(metrics).build().start(); // starts reporting via JMX
+
         final Channel<Object> ch = Channels.newChannel(0);
 
-        // create and register MBean
-        ManagementFactory.getPlatformMBeanServer().registerMBean(new JModernInfo() {
-            @Override
-            public void send(String message) {
-                try {
-                    ch.send(message);
-                } catch (Exception e) {
-                    throw new RunTimeException(e);
-                }
-            }
-
-            @Override
-            public int getNumMessagesReceived() {
-                return counter.get();
-            }
-        }, new ObjectName("jmodern:type=Info"));
-
         new Fiber<Void>(() -> {
-            for (int i = 0; i < 1000000; i++) {
-                Strand.sleep(100);
-                log.info("Sending {}", i); //log something
+            Meter meter = metrics.meter(name(Main.class, "messages" , "send", "rate"));
+            for (int i = 0; i < 100000; i++) {
+                Strand.sleep(ThreadLocalRandom.current().nextInt(50, 500)); // random sleep
+                meter.mark(); // measures event rate
+
                 ch.send(i);
-                if (i % 10 == 0)
-                    log.warn("Send {} messages", i + 1); // log something
             }
             ch.close();
         }).start();
 
         new Fiber<Void>(() -> {
-            Integer x;
-            while((x = ch.receive()) != null)
-                System.out.println("--> " + x);
-        }).start().join(); // join waits for this fiber to finish
-    }
+            Counter counter = metrics.counter(name(Main.class, "messages", "received"));
+            Timer timer = metrics.timer(name(Main.class, "messages", "duration"));
 
-    @MXBean
-    public interface JModernInfo {
-        void send(String messaage);
-        int getNumMessagesReceived();
+            Object x;
+            long lastReceived = System.nanoTime();
+            while ((x = ch.receive()) != null) {
+                final long now = System.nanoTime();
+                timer.update(now - lastReceived, NANOSECONDS); // creates duration histogram
+                lastReceived = now;
+                counter.inc(); // counts
+
+                System.out.println("--> " + x);
+            }
+        }).start().join(); // join waits for this fiber to finish
     }
 }
