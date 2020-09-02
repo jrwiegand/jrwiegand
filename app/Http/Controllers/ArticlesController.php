c8b8194532a7de843e8186eccec55db4d56a8e94
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-02 16:17:38 -0400

    05.05 form validation essentials

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -28,6 +28,13 @@ class ArticlesController extends Controller
 
     public function store()
     {
+
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'
+        ]);
+
         $article = new Article();
         $article->title = request('title');
         $article->excerpt = request('excerpt');
@@ -46,6 +53,12 @@ class ArticlesController extends Controller
 
     public function update($id)
     {
+        request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'
+        ]);
+
         $article = Article::find($id);
 
         $article->title = request('title');
