1c713cf389b3e38583bb4ee4451fdff5ac5e5f3a
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-02 16:05:59 -0400

    05.04 forms that submit put requests

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -28,9 +28,6 @@ class ArticlesController extends Controller
 
     public function store()
     {
-        // validation
-
-        // clean up
         $article = new Article();
         $article->title = request('title');
         $article->excerpt = request('excerpt');
@@ -39,5 +36,23 @@ class ArticlesController extends Controller
         $article->save();
 
         return redirect('/articles');
+    }
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('articles.edit', compact('article'));
+    }
+
+    public function update($id)
+    {
+        $article = Article::find($id);
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles/' . $article->id);
     }
 }
