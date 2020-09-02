a0643bcc65d2a75596258fc7ee1031551d80ebb1
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-02 09:57:57 -0400

    03.01 setup a database connection

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,23 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PostsController extends Controller
 {
-    public function show($post)
+    public function show($slug)
     {
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-
-        if (!array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
+        $post = \DB::table('posts')->where('slug', $slug)->first();
 
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
