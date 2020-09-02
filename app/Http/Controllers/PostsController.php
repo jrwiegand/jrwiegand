9b66568d24f3b12e1b9b60e3596d20f13fe0ff4c
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-02 10:10:09 -0400

    03.02 hello eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use App\Post;
+
 class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
