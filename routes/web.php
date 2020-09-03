d339dc9f8f77292173f1c9f599a4e0ee8c29936d
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-03 09:48:51 -0400

    08.01 build a registration system in mere minutes

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view ('about', [
-        'articles' => App\Article::take(3)->latest()->get()
-    ]);
-});
+Auth::routes();
 
-Route::get('/articles', 'ArticlesController@index')->name('articles.index');
-Route::post('/articles', 'ArticlesController@store');
-Route::get('/articles/create', 'ArticlesController@create');
-Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
-Route::get('/articles/{article}/edit', 'ArticlesController@edit');
-Route::put('/articles/{article}', 'ArticlesController@update');
+Route::get('/home', 'HomeController@index')
+    ->name('home')
+    ->middleware('auth');
