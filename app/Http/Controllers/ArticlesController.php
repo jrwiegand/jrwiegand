35e060c2ce9090539e765dc94ff9da8290276ec2
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-02 23:11:02 -0400

    07.05 attach and validate many-to-many inserts

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Tag;
-use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
@@ -26,12 +25,20 @@ class ArticlesController extends Controller
 
     public function create()
     {
-        return view('articles.create');
+        return view('articles.create', [
+            'tags' => Tag::all()
+        ]);
     }
 
     public function store()
     {
-        Article::create($this->validateArticle());
+        $this->validateArticle();
+
+        $article = new Article(request(['title', 'excerpt', 'body']));
+        $article->user_id = 1; // auth()->id()
+        $article->save();
+
+        $article->tags()->attach(request('tags'));
 
         return redirect(route('articles.index'));
     }
@@ -52,7 +59,8 @@ class ArticlesController extends Controller
         return request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'tags' => 'exists:tags,id'
         ]);
     }
 }
--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -17,7 +17,7 @@
                 <div class="control">
                     <input class="input @error('title') is-danger @enderror" type="text" name="title" id="title" value="{{ old('title') }}">
                     @error('title')
-                    <p class="help is-danger">{{ $errors->first('title') }}</p>
+                    <p class="help is-danger">{{ $message }}</p>
                     @enderror
                 </div>
             </div>
@@ -27,7 +27,7 @@
                 <div class="control">
                     <textarea class="textarea @error('excerpt') is-danger @enderror" name="excerpt" id="excerpt">{{ old('excerpt') }}</textarea>
                     @error('excerpt')
-                    <p class="help is-danger">{{ $errors->first('excerpt') }}</p>
+                    <p class="help is-danger">{{ $message }}</p>
                     @enderror
                 </div>
             </div>
@@ -37,7 +37,21 @@
                 <div class="control">
                     <textarea class="textarea @error('body') is-danger @enderror" name="body" id="body">{{ old('body') }}</textarea>
                     @error('body')
-                    <p class="help is-danger">{{ $errors->first('body') }}</p>
+                    <p class="help is-danger">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label" for="tags[]">Tags</label>
+                <div class="select is-multiple control">
+                    <select name="tags[]" multiple>
+                        @foreach($tags as $tag)
+                        <option value="{{$tag->id }}">{{ $tag->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('tags')
+                    <p class="help is-danger">{{ $message }}</p>
                     @enderror
                 </div>
             </div>
