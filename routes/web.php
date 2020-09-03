3d00299061b5bc41a8efc77811ac6731677214ed
Joshua Wiegand <josh.r.wiegand@gmail.com>
2020-09-03 15:40:02 -0400

    09.04 automatically resolve dependencies

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-
-    $container = new \App\Container();
-
-    $container->bind('example', function() {
-        return new \App\Example();
-    });
-
-    $example = $container->resolve('example');
-
-    $example->go();
-
-});
+Route::get('/', 'PagesController@home');
