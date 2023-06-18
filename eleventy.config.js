efc8adbeef22e8d6063131aed483f390bcbdf58b
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-06-18 14:07:14 -0400

    feat: base web component setup, complete

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,18 @@ const pluginWebc = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (config) {
   config.addPlugin(pluginWebc, {
-    components: "_includes/webc/*.webc",
+    components: "src/_includes/components/*.webc",
   });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+    passthroughFileCopy: true,
+    templateFormats: ["html", "md", "webc"],
+    htmlTemplateEngine: "webc",
+    dataTemplateEngine: "webc",
+    markdownTemplateEngine: "webc",
+  };
 };
