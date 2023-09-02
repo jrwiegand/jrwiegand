1ac0ba5951ae2f6ed254497cc941e4171fa529ed
Joshua Wiegand <jwiegand@stansberryresearch.com>
2023-09-02 14:45:57 -0400

    chore(lint): run lint on the whole project

--- web/apps/josh-wgnd-org/svelte.config.js
+++ web/apps/josh-wgnd-org/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,8 +14,8 @@ const config = {
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
+      pages: "build",
+      assets: "build",
       fallback: undefined,
       precompress: false,
       strict: false,
