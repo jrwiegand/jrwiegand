56214d29cf9b4653d1cd0a4792297ed8ed737967
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-09-19 23:28:53 -0400

    chore: remove test app

--- web/apps/new-edge-web/src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
-<div>
-	<h1>About</h1>
-	<p>My very own place to talk about myself.</p>
-</div>
