8eda76fcbd28b60c93b30fadb3d170af45de8e19
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-10-07 15:59:03 -0400

    learn: svelte - reactivity - statements
    https://learn.svelte.dev/tutorial/reactive-statements

--- explore/bun-kit/src/routes/+page.svelte
+++ explore/bun-kit/src/routes/+page.svelte
@@ -1,15 +1,22 @@
 <script>
 	let count = 0;
-    $: doubled = count * 2;
+    $: {
+        console.log(`the count is ${count}`);
+        console.log(`this will also be logged whenever count changes`);
+    }
 
-	function increment() {
+    $: if (count >= 10) {
+        alert('count is dangerously high!');
+        count = 0;
+    }
+
+
+	function handleClick() {
 		count += 1;
 	}
 </script>
 
-<button on:click={increment}>
+<button on:click={handleClick}>
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
-
-<p>{count} doubled is {doubled}</p>
