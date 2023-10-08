2debe5ecfc561f45279b847fcd692efc99c823b1
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-10-08 15:32:10 -0400

    learn: svelte - props - spread props
    https://learn.svelte.dev/tutorial/spread-props

--- explore/bun-kit/src/routes/+page.svelte
+++ explore/bun-kit/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script>
-	import Nested from './Nested.svelte';
+	import PackageInfo from './PackageInfo.svelte';
+	const pkg = {
+		name: 'svelte',
+		speed: 'blazing',
+		version: 4,
+		website: 'https://svelte.dev'
+	};
 </script>
 
-<Nested answer={42} />
-<Nested />
+<PackageInfo {...pkg} />
