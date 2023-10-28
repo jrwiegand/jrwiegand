d7acf8c0f851eaf40e3869c1fff57ef13036340b
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-10-28 13:53:50 -0400

    learn: svelte - stores - derived stores
    https://learn.svelte.dev/tutorial/derived-stores

--- explore/bun-kit/src/routes/+page.svelte
+++ explore/bun-kit/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { time } from './stores.js';
+	import { time, elapsed } from './stores.js';
 
 	const formatter = new Intl.DateTimeFormat(
 		'en',
@@ -13,3 +13,9 @@
 </script>
 
 <h1>The time is {formatter.format($time)}</h1>
+
+<p>
+	This page has been open for
+	{$elapsed}
+	{$elapsed === 1 ? 'second' : 'seconds' }
+</p>
--- explore/bun-kit/src/routes/stores.js
+++ explore/bun-kit/src/routes/stores.js
@@ -1,11 +1,15 @@
-import { readable } from 'svelte/store';
+import { derived, readable } from 'svelte/store';
 
 export const time = readable(new Date(), function start(set) {
-    const interval = setInterval(() => {
-        set(new Date());
-    }, 1000);
+	const interval = setInterval(() => {
+		set(new Date());
+	}, 1000);
 
-    return function stop() {
-        clearInterval(interval);
-    };
+	return function stop() {
+		clearInterval(interval);
+	};
 });
+
+const start = new Date();
+
+export const elapsed = derived(time, ($time) => $time.getSeconds() - start.getSeconds());
