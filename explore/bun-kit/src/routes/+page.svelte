3785a0135de85cbf77f5df04f1b1c8836ea5b7e2
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-12-09 16:04:43 -0500

    learn: svelte - special elements - <svelte:head>
    https://learn.svelte.dev/tutorial/svelte-head

--- explore/bun-kit/src/routes/+page.svelte
+++ explore/bun-kit/src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-	let selection: Selection | null = null;
-
-	const handleSelectionChange = () => selection = document.getSelection();
+	const themes = ['margaritaville', 'retrowave', 'spaaaaace', 'halloween'];
+	let selected = themes[0];
 </script>
 
-<svelte:document on:selectionchange={handleSelectionChange}/>
+<svelte:head>
+	<link rel="stylesheet" href="/stylesheets/{selected}.css" />
+</svelte:head>
 
-<h1>Select this text to fire events</h1>
-<p>Selection: {selection ? selection.toString() : ''}</p>
+<h1>Welcome to my site!</h1>
+
+<select bind:value={selected}>
+	<option disabled>choose a theme</option>
+
+	{#each themes as theme}
+		<option>{theme}</option>
+	{/each}
+</select>
