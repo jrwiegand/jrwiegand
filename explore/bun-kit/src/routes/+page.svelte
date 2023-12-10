40ea6a776ab8d713546e7c800bd9789c294d44b9
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-12-10 14:24:10 -0500

    learn: svelte - next steps - congratulations
    https://learn.svelte.dev/tutorial/congratulations

--- explore/bun-kit/src/routes/+page.svelte
+++ explore/bun-kit/src/routes/+page.svelte
@@ -1,20 +1,55 @@
 <script>
-	let user = {
-		firstname: 'Ada',
-		lastname: 'Lovelace'
-	};
+	import { onMount } from 'svelte';
+
+	let characters = ['🥳', '🎉', '✨'];
+
+	let confetti = new Array(100)
+		.fill()
+		.map((_, i) => {
+			return {
+				character:
+					characters[i % characters.length],
+				x: Math.random() * 100,
+				y: -20 - Math.random() * 100,
+				r: 0.1 + Math.random() * 1
+			};
+		})
+		.sort((a, b) => a.r - b.r);
+
+	onMount(() => {
+		let frame;
+
+		function loop() {
+			frame = requestAnimationFrame(loop);
+
+			confetti = confetti.map((emoji) => {
+				emoji.y += 0.3 * emoji.r;
+				if (emoji.y > 120) emoji.y = -20;
+				return emoji;
+			});
+		}
+
+		loop();
+
+		return () => cancelAnimationFrame(frame);
+	});
 </script>
 
-<label>
-	<input bind:value={user.firstname} />
-	first name
-</label>
+{#each confetti as c}
+	<span
+		style="left: {c.x}%; top: {c.y}%; transform: scale({c.r})"
+		>{c.character}</span
+	>
+{/each}
 
-<label>
-	<input bind:value={user.lastname} />
-	last name
-</label>
+<style>
+	:global(body) {
+		overflow: hidden;
+	}
 
-{@debug user}
-
-<h1>Hello {user.firstname}!</h1>
+	span {
+		position: absolute;
+		font-size: 5vw;
+		user-select: none;
+	}
+</style>
