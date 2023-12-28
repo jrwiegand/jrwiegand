70b34cb8d5e3a5a9e67399b4aa9f99be10f00d8a
Josh Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-12-28 14:38:36 +0000

    learn: sveltekit - errors and redirects - basics
    https://learn.svelte.dev/tutorial/error-basics

--- explore/bun-kit/src/routes/+layout.svelte
+++ explore/bun-kit/src/routes/+layout.svelte
@@ -1,53 +1,7 @@
-<script>
-	import { page, navigating, updated } from '$app/stores';
-</script>
-
 <nav>
-	<a href="/" aria-current={$page.url.pathname === '/'}>
-		home
-	</a>
-
-	<a href="/about" aria-current={$page.url.pathname === '/about'}>
-		about
-	</a>
-
-	{#if $navigating}
-		navigating to {$navigating.to.url.pathname}
-	{/if}
+	<a href="/">home</a>
+	<a href="/expected">page with expected error</a>
+	<a href="/unexpected">page with unexpected error</a>
 </nav>
 
 <slot />
-
-{#if $updated}
-	<div class="toast">
-		<p>
-			A new version of the app is available
-
-			<button on:click={() => location.reload()}>
-				reload the page
-			</button>
-		</p>
-	</div>
-{/if}
-
-<style>
-	.toast {
-		position: fixed;
-		left: 0;
-		bottom: 0;
-		width: 100%;
-		padding: 1rem;
-		display: flex;
-		justify-content: center;
-		gap: 1rem;
-	}
-
-	.toast p {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		background: var(--bg-2);
-		padding: 0.5rem 0.5rem 0.5rem 1rem;
-		border-radius: 4px;
-	}
-</style>
