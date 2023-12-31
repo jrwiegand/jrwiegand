d5ca6ad6a334642c5cf746c4a1cba1dac00efafd
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2023-12-30 20:34:29 -0500

    learn: sveltekit - hooks - event
    https://learn.svelte.dev/tutorial/event

--- explore/bun-kit/src/hooks.server.ts
+++ explore/bun-kit/src/hooks.server.ts
@@ -1,12 +1,4 @@
 export async function handle({ event, resolve }) {
-	if (event.url.pathname === '/ping') {
-		return new Response('pong');
-	}
-
-	return await resolve(event, {
-		transformPageChunk: ({ html }) => html.replace(
-			'<body',
-			'<body style="color: hotpink"'
-		)
-	});
+	event.locals.answer = 42;
+	return await resolve(event);
 }
