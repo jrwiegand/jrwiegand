fc50aaf861aee9fa4c5ad2e2cddf0151b6bc40ae
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-03 06:20:39 -0500

    learn: sveltekit - page options - csr
    https://learn.svelte.dev/tutorial/csr

--- sites/bunkit.wgnd.org/src/routes/+page.svelte
+++ sites/bunkit.wgnd.org/src/routes/+page.svelte
@@ -1,1 +1,15 @@
-<h1>{window.innerWidth}x{window.innerHeight}</h1>
+<script>
+	import { browser } from '$app/environment';
+
+	let count = 0;
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<h1>Rendered {browser ? 'in the browser' : 'on the server'}</h1>
+
+<button on:click={increment}>
+	Clicks: {count}
+</button>
