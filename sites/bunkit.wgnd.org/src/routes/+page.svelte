e8573e34d4e8cf87967a8f8f8ef38db2fd26e0e0
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-04 22:32:56 -0500

    learn: sveltekit - page options - trailingslash
    https://learn.svelte.dev/tutorial/trailingslash

--- sites/bunkit.wgnd.org/src/routes/+page.svelte
+++ sites/bunkit.wgnd.org/src/routes/+page.svelte
@@ -1,15 +1,1 @@
-<script>
-	import { browser } from '$app/environment';
-
-	let count = 0;
-
-	function increment() {
-		count += 1;
-	}
-</script>
-
-<h1>Rendered {browser ? 'in the browser' : 'on the server'}</h1>
-
-<button on:click={increment}>
-	Clicks: {count}
-</button>
+<h1>trailingSlash</h1>
