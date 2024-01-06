c998108aaca3d22fb1221089692be8a2e1ebdea8
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-05 21:10:04 -0500

    learn: sveltekit - link options - preloading
    https://learn.svelte.dev/tutorial/preload

--- sites/bunkit.wgnd.org/src/routes/+layout.svelte
+++ sites/bunkit.wgnd.org/src/routes/+layout.svelte
@@ -1,10 +1,26 @@
+<script lang="ts">
+	import { navigating } from "$app/stores";
+
+	let previous: { from: { url: URL }, to: { url: URL } };
+	let start: number;
+	let end: number;
+
+	$: if ($navigating) {
+		start = Date.now();
+		end = 0;
+		previous = $navigating;
+	} else {
+		end = Date.now();
+	}
+</script>
 <nav>
-	<a href="/always">/always</a>
-	<a href="/always/">/always/</a>
-	<a href="/ignore">/ignore</a>
-	<a href="/ignore/">/ignore/</a>
-	<a href="/never">/never</a>
-	<a href="/never/">/never/</a>
+	<a href="/">home</a>
+	<a href="/slow-a" data-sveltekit-preload-data>/slow-a</a>
+	<a href="/slow-b">/slow-b</a>
 </nav>
 
 <slot />
+
+{#if previous && end}
+	<p>navigated from {previous.from.url.pathname} to {previous.to.url.pathname} in <strong>{end - start}ms</strong></p>
+{/if}
--- sites/bunkit.wgnd.org/src/routes/+page.svelte
+++ sites/bunkit.wgnd.org/src/routes/+page.svelte
@@ -1,1 +1,1 @@
-<h1>trailingSlash</h1>
+<h1>home</h1>
