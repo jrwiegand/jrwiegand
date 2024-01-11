0dff189152c0c771bd73cfa436a4e4b2d984bc2a
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-10 22:50:03 -0500

    site(jw): start putting together the content

--- sites/josh.wgnd.org/src/routes/+page.svelte
+++ sites/josh.wgnd.org/src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<h1>Hello.</h1>
+	<h2>My name is Josh Wiegand.</h2>
+
+	<img src="/profile.png" alt="profile" />
+
+	<p>
+		I craft computer bits and bytes to help people. I enjoy learning and talking many topics
+		including new tech, movies, lawn care, house projects, etc.
+	</p>
+
+	<p>
+		You can follow me on <a
+			href="https://twitter.com/joshrwiegand"
+			target="_blank"
+			rel="noopener noreferrer">Twitter</a
+		>
+		or
+		<a href="https://www.instagram.com/josh.wiegand/" target="_blank" rel="noopener noreferrer"
+			>Instagram</a
+		>
+		where I infrequently post about anything. I also do my best to stay active on
+		<a href="https://github.com/jrwiegand" target="_blank" rel="noopener noreferrer">GitHub</a>,
+		opening the occasional PR.
+	</p>
+
+	<p>
+		When I am not programming, I am likely helping my wife raise our two kids, playing or watching
+		sports (especially basketball), fixing a thing, taking in a movie, mowing the grass, reading a
+		book or doing something else that is vitally important for the next five minutes.
+	</p>
+</main>
