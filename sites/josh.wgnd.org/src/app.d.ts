a34f47a57a048a75f8ef10d2f2cfaa9be593c7bd
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-12 13:02:08 -0500

    feat(cloudflare): finish up configuration

--- sites/josh.wgnd.org/src/app.d.ts
+++ sites/josh.wgnd.org/src/app.d.ts
@@ -2,12 +2,16 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+        interface Platform {
+          env: {
+            COUNTER: DurableObjectNamespace;
+          };
+          context: {
+            waitUntil(promise: Promise<any>): void;
+          };
+          caches: CacheStorage & { default: Cache }
+        }
+    }
 }
 
 export {};
