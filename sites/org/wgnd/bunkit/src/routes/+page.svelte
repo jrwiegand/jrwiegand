c8698dc4ab3459b5481711f10567312566f93b11
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-16 20:51:37 -0500

    learn: sveltekit - advanced loading - using parent data
    https://learn.svelte.dev/tutorial/await-parent

--- sites/org/wgnd/bunkit/src/routes/+page.svelte
+++ sites/org/wgnd/bunkit/src/routes/+page.svelte
@@ -1,5 +1,2 @@
-<script lang="ts">
-    export let data;
-</script>
-
-<svelte:component this={data.component} message={data.message} />
+<p>if a = 1 and b = a + 1, what is a + b?</p>
+<a href="/sum">show answer</a>
--- sites/org/wgnd/bunkit/src/routes/BoringComponent.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    export let message: string;
-</script>
-
-<h1>{message}</h1>
