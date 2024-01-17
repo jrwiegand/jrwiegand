4d369cf7f2e1ade00f1cdb1d1b3793a726726744
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-01-16 21:07:43 -0500

    learn: sveltekit - advanced loading - invalidation
    https://learn.svelte.dev/tutorial/invalidation

---
+++ sites/org/wgnd/bunkit/src/routes/[...timezone]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { invalidate } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  export let data;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      invalidate("/api/now");
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<h1>
+  {new Intl.DateTimeFormat([], {
+    timeStyle: "full",
+    timeZone: data.timezone,
+  }).format(new Date(data.now))}
+</h1>
--- sites/org/wgnd/bunkit/src/routes/sum/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-  export let data;
-</script>
-
-<p>{data.a} + {data.b} = {data.c}</p>
-<p><a href="/">home</a></p>
