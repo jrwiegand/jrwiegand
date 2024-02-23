1a9a11a714365b0530ff2460ccc996739e7ad1a4
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-02-22 22:52:59 -0500

    feat(sites): init a `/uses` page

---
+++ sites/org/wgnd/josh/src/routes/uses/+page.svelte
@@ -0,0 +1,21 @@
+<main>
+	<h1>Uses</h1>
+	<p>All the (interesting) things I use.</p>
+	<p>Inspired by the many uses pages found on <a href="https://uses.tech/" target="_blank" rel="noopener noreferrer">uses.tech</a>.</p>
+	<h2>Hardware</h2>
+	<ul>
+		<li>iMac (Retina 5K, 27-inch, 2017)</li>
+		<li>MacBook Pro (15-inch, 2019)</li>
+		<li>MX Master 3</li>
+		<li>iPhone 13 Pro</li>
+	</ul>
+	<h2>Software</h2>
+	<ul>
+		<li>Obsidian</li>
+		<li>TickTick</li>
+		<li>1Password</li>
+		<li>Visual Studio Code</li>
+		<li>iTerm2</li>
+		<li>YNAB</li>
+	</ul>
+</main>
