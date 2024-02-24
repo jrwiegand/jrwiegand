d977dfc04f5f2387b27746a0c98360720ba408c3
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-02-24 16:09:23 -0500

    feat(sites): create a now page

---
+++ sites/org/wgnd/josh/src/routes/now/+page.svelte
@@ -0,0 +1,48 @@
+<main>
+	<h1>Now</h1>
+
+	<p>What I am up to now.</p>
+	<p>
+		Inspired by the many now pages found on <a
+			href="https://now.garden/"
+			target="_blank"
+			rel="noopener noreferrer">now.garden</a
+		>.
+	</p>
+
+	<h2>Making</h2>
+	<ul>
+		<li>This website!</li>
+	</ul>
+
+	<h2>Learning</h2>
+	<ul>
+		<li>Machine Learning</li>
+		<li>Distributed Systems</li>
+		<li>Rust Lang</li>
+	</ul>
+
+	<h2>Reading</h2>
+	<ul>
+		<li>
+			<a
+				href="https://irp.fas.org/doddir/army/adp6_22.pdf"
+				target="_blank"
+				rel="noopener noreferrer">Army Leadership and the Profession</a
+			>
+		</li>
+		<li>Software Engineering at Google</li>
+	</ul>
+
+	<h2>Listening</h2>
+	<ul>
+		<li>Many Podcasts</li>
+	</ul>
+
+	<h2>Watching</h2>
+	<ul>
+		<li>Masters of the Air</li>
+	</ul>
+
+	<p><strong><em>Updated: 2024-02-24</em></strong></p>
+</main>
