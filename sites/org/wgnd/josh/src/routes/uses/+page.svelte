e69f8518e3fc7453dcdf3c041336f72568caa38c
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2024-02-24 16:09:44 -0500

    chore(sites): add more things I use

--- sites/org/wgnd/josh/src/routes/uses/+page.svelte
+++ sites/org/wgnd/josh/src/routes/uses/+page.svelte
@@ -1,14 +1,28 @@
 <main>
 	<h1>Uses</h1>
-	<p>All the (interesting) things I use.</p>
-	<p>Inspired by the many uses pages found on <a href="https://uses.tech/" target="_blank" rel="noopener noreferrer">uses.tech</a>.</p>
+
+	<p>The (interesting) things I use.</p>
+	<p>
+		Inspired by the many uses pages found on <a
+			href="https://uses.tech/"
+			target="_blank"
+			rel="noopener noreferrer">uses.tech</a
+		>.
+	</p>
+
 	<h2>Hardware</h2>
 	<ul>
 		<li>iMac (Retina 5K, 27-inch, 2017)</li>
 		<li>MacBook Pro (15-inch, 2019)</li>
 		<li>MX Master 3</li>
 		<li>iPhone 13 Pro</li>
+		<li>Apple TV 4K</li>
+		<li>Netgate 2100</li>
+		<li>U6 Pro</li>
+		<li>USW Lite 8 PoE</li>
+		<li>USW Flex Mini</li>
 	</ul>
+
 	<h2>Software</h2>
 	<ul>
 		<li>Obsidian</li>
@@ -17,5 +31,8 @@
 		<li>Visual Studio Code</li>
 		<li>iTerm2</li>
 		<li>YNAB</li>
+		<li>Tailscale</li>
 	</ul>
+
+	<p><strong><em>Updated: 2024-02-24</em></strong></p>
 </main>
