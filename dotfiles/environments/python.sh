73eb11d0071cfbdc935123f2293d7b7b0e39aa98
Joshua Wiegand <jwiegand@stansberryresearch.com>
2024-07-08 16:56:02 -0400

    dotfiles: remove brew carveout

--- dotfiles/environments/python.sh
+++ dotfiles/environments/python.sh
@@ -1,8 +1,4 @@
 if [ -d "$HOME/.pyenv" ]; then
-	## To avoid them accidentally linking against a Pyenv-provided Python
-	## https://github.com/pyenv/pyenv#getting-pyenv
-	alias brew='env PATH="${PATH//$(pyenv root)\/shims:/}" brew'
-
 	export PYENV_ROOT="$HOME/.pyenv"
 	[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 	eval "$(pyenv init -)"
