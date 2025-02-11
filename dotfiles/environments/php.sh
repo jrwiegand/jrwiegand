79c28c64d65b5b90c52b48002a9c852f7549b57b
Joshua Wiegand <1606617+jrwiegand@users.noreply.github.com>
2025-02-10 20:37:27 -0500

    chore: check for composer in new location

--- dotfiles/environments/php.sh
+++ dotfiles/environments/php.sh
@@ -4,4 +4,9 @@ if type composer > /dev/null; then
 		export PATH="$HOME/.composer/vendor/bin:$PATH"
 		export UPDATE_COMPOSER=true
 	fi
+
+	if [ -d "$HOME/.config/composer" ]; then
+		export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+		export UPDATE_COMPOSER=true
+	fi
 fi
