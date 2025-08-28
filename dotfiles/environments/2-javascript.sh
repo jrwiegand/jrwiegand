415ba4a574ca940304de15f20ba01ee3ea94f9ac
Joshua Wiegand <josh@wgnd.org>
2025-08-27 22:23:17 -0400

    refactor: remove volta add fnm

--- dotfiles/environments/2-javascript.sh
+++ dotfiles/environments/2-javascript.sh
@@ -5,11 +5,22 @@ if type bun > /dev/null; then
 	export UPDATE_BUN=true
 fi
 
-export UPDATE_VOLTA= false
-if [ -d "$HOME/.volta" ]; then
-	export VOLTA_HOME="$HOME/.volta"
-	export PATH="$VOLTA_HOME/bin:$PATH"
-	export UPDATE_VOLTA=true
+export UPDATE_FNM=false
+if type fnm > /dev/null; then
+	export PATH="/Users/jwiegand/.local/state/fnm_multishells/94116_1756347173764/bin":$PATH
+	export FNM_MULTISHELL_PATH="/Users/jwiegand/.local/state/fnm_multishells/94116_1756347173764"
+	export FNM_VERSION_FILE_STRATEGY="local"
+	export FNM_DIR="/Users/jwiegand/.local/share/fnm"
+	export FNM_LOGLEVEL="info"
+	export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+	export FNM_COREPACK_ENABLED="false"
+	export FNM_RESOLVE_ENGINES="true"
+	export FNM_ARCH="arm64"
+	rehash
+
+	eval "$(fnm env --use-on-cd --shell zsh)"
+
+	export UPDATE_FNM=true
 fi
 
 export UPDATE_NVM=false
